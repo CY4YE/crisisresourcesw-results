182bbfa38aa88ce9152af523045cb12f1307240c
yak-fumblepack <liang.mike.to@gmail.com>
2021-07-24 20:50:47 -0400

    add in Ashton's css for the homepage, and html for the result boxes

--- crisisresources-main/utils/search.js
+++ crisisresources-main/utils/search.js
@@ -23,12 +23,10 @@ const fuse = new Fuse(list, options);
 const searchElement = document.getElementById("search-input")
 const resultSimilar = document.getElementById("results-container-similar")
 const resultAccurate = document.getElementById("results-container-accurate")
-const rawJson = document.getElementById("raw-json")
 
 const searchRegions = () => {
   const resultJSON = fuse.search(searchElement.value);
   resultAccurate.innerHTML = `<a href="${resultJSON[0].item.url}">${resultJSON[0].item.title}</a>`
-  rawJson.innerHTML = JSON.stringify(resultJSON, null, 4);
   resultSimilar.innerHTML = `1. <a href="${resultJSON[1].item.url}">${resultJSON[1].item.title}</a>\n2. <a href="${resultJSON[2].item.url}">${resultJSON[2].item.title}</a>\n3. <a href="${resultJSON[3].item.url}">${resultJSON[3].item.title}</a>`;
 }
 
