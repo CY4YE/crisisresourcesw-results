b8c0f601684c057fd4f9ba2a3a64727a1d6ac3b6
SerpentDerpady <42683399+SerpentDerpady@users.noreply.github.com>
2021-08-18 13:49:48 -0300

    Delete crisisresources-main directory

--- crisisresources-main/utils/markdown.js
+++
@@ -1,9 +0,0 @@
-const convertMarkdown = () => {
-    let markdown_text = document.getElementById("markdown-content");
-    const converter = new showdown.Converter();
-    converter.setFlavor("github");
-    let html = converter.makeHtml(markdown_text.innerHTML);
-    console.log(html)
-    markdown_text.innerHTML = html
-}
-convertMarkdown()
