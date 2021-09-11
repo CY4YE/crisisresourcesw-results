362bbdd42b556be9c8dfadf209b12add9aae2543
yak-fumblepack <liang.mike.to@gmail.com>
2021-09-11 15:01:56 -0400

    add in crisis resources for alberta

--- crisisresourcesw-results-main/crisisresources-main/utils/regions_data.js
+++ crisisresourcesw-results-main/crisisresources-main/utils/regions_data.js
@@ -348,4 +348,174 @@ const regionsData = [
     "date": "2021-07-01 00:00:00 -0400",
     "description": ""
   },
+  // Alberta
+  /* =================================================== */
+  {
+    "title": "Calgary",
+    "url": "region/Alberta/calgary.html",
+    "category": "",
+    "tags": {
+      "tag1": "Alberta",
+      "tag2": "Calgary",
+      "tag3": "Calgary Metropolitan Region",
+      "tag4": "",
+      "tag5": "",
+      "tag6": ""
+    },
+    "date": "2021-07-01 00:00:00 -0400",
+    "description": ""
+  },
+  {
+    "title": "Banff/Canmore",
+    "url": "region/Alberta/banffcanmore.html",
+    "category": "",
+    "tags": {
+      "tag1": "Alberta",
+      "tag2": "Banff",
+      "tag3": "Canmore",
+      "tag4": "",
+      "tag5": "",
+      "tag6": ""
+    },
+    "date": "2021-07-01 00:00:00 -0400",
+    "description": ""
+  },
+  {
+    "title": "Cold Lake",
+    "url": "region/Alberta/coldlake.html",
+    "category": "",
+    "tags": {
+      "tag1": "Alberta",
+      "tag2": "Cold Lake",
+      "tag3": "",
+      "tag4": "",
+      "tag5": "",
+      "tag6": ""
+    },
+    "date": "2021-07-01 00:00:00 -0400",
+    "description": ""
+  },
+  {
+    "title": "Edmonton",
+    "url": "region/Alberta/edmonton.html",
+    "category": "",
+    "tags": {
+      "tag1": "Alberta",
+      "tag2": "Edmonton",
+      "tag3": "Edmonton Metropolitan Region",
+      "tag4": "",
+      "tag5": "",
+      "tag6": ""
+    },
+    "date": "2021-07-01 00:00:00 -0400",
+    "description": ""
+  },
+  {
+    "title": "Fort McMurray",
+    "url": "region/Alberta/fortmcmurray.html",
+    "category": "",
+    "tags": {
+      "tag1": "Alberta",
+      "tag2": "Fort McMurray",
+      "tag3": "",
+      "tag4": "",
+      "tag5": "",
+      "tag6": ""
+    },
+    "date": "2021-07-01 00:00:00 -0400",
+    "description": ""
+  },
+  {
+    "title": "Grande Prairie",
+    "url": "region/Alberta/grandeprairie.html",
+    "category": "",
+    "tags": {
+      "tag1": "Alberta",
+      "tag2": "Grande Prairie",
+      "tag3": "",
+      "tag4": "",
+      "tag5": "",
+      "tag6": ""
+    },
+    "date": "2021-07-01 00:00:00 -0400",
+    "description": ""
+  },
+  {
+    "title": "Lethbridge",
+    "url": "region/Alberta/lethbridge.html",
+    "category": "",
+    "tags": {
+      "tag1": "Alberta",
+      "tag2": "Lethbridge",
+      "tag3": "",
+      "tag4": "",
+      "tag5": "",
+      "tag6": ""
+    },
+    "date": "2021-07-01 00:00:00 -0400",
+    "description": ""
+  },
+  {
+    "title": "Lloydminster",
+    "url": "region/Alberta/lloydminster.html",
+    "category": "",
+    "tags": {
+      "tag1": "Alberta",
+      "tag2": "Lloydminster",
+      "tag3": "",
+      "tag4": "",
+      "tag5": "",
+      "tag6": ""
+    },
+    "date": "2021-07-01 00:00:00 -0400",
+    "description": ""
+  },
+
+  {
+    "title": "Medicine Hat",
+    "url": "region/Alberta/medicinehat.html",
+    "category": "",
+    "tags": {
+      "tag1": "Alberta",
+      "tag2": "Medicine Hat",
+      "tag3": "Medicine Hat Metropolitan Region",
+      "tag4": "",
+      "tag5": "",
+      "tag6": ""
+    },
+    "date": "2021-07-01 00:00:00 -0400",
+    "description": ""
+  },
+
+  {
+    "title": "Red Deer",
+    "url": "region/Alberta/reddeer.html",
+    "category": "",
+    "tags": {
+      "tag1": "Alberta",
+      "tag2": "Red Deer",
+      "tag3": "Red Deer Metropolitan Region",
+      "tag4": "",
+      "tag5": "",
+      "tag6": ""
+    },
+    "date": "2021-07-01 00:00:00 -0400",
+    "description": ""
+  },
+
+  {
+    "title": "Whitecourt",
+    "url": "region/Alberta/whitecourt.html",
+    "category": "",
+    "tags": {
+      "tag1": "Alberta",
+      "tag2": "Whitecourt",
+      "tag3": "",
+      "tag4": "",
+      "tag5": "",
+      "tag6": ""
+    },
+    "date": "2021-07-01 00:00:00 -0400",
+    "description": ""
+  },
 ]
